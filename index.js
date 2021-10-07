b5bfcb457be288cfaacf65c7dde32c3de53c2a9e
kalaiy21 <kalaiy2132002@gmail.com>
2021-10-07 12:01:37 +0530

    second commit

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-var prevScrollpos = window.pageYOffset;
+// var prevScrollpos = window.pageYOffset;
 
-window.onscroll = function () {
+// window.onscroll = function () {
 
-  var currentScrollPos = window.pageYOffset;
+//   var currentScrollPos = window.pageYOffset;
 
-  if (prevScrollpos > currentScrollPos) {
-    $("#navbar").css({"top":"0"});
-  } 
-  else {
-    $("#navbar").css({"top":"-196px", "background-color" : "#d0fcda","box-shadow":"0px 14px 30px 2px rgba(0,0,0,0.20)"});
-    $(".navitem").css("color","black");
-  }
-  prevScrollpos = currentScrollPos;
+//   if (prevScrollpos > currentScrollPos) {
+//     $("#navbar").css({"top":"0"});
+//   } 
+//   else {
+//     $("#navbar").css({"top":"-196px", "background-color" : "#d0fcda","box-shadow":"0px 14px 30px 2px rgba(0,0,0,0.20)"});
+//     $(".navitem").css("color","black");
+//   }
+//   prevScrollpos = currentScrollPos;
 
-  if(prevScrollpos == 0){
-    $("#navbar").css({"top":"0", "background-color" : "transparent","box-shadow":"none"});
-    $(".navitem").css("color","white");
-  }
+//   if(prevScrollpos == 0){
+//     $("#navbar").css({"top":"0", "background-color" : "transparent","box-shadow":"none"});
+//     $(".navitem").css("color","white");
+//   }
 
-}
+// }
+
+
+$(".ham-menu").click(function(){
+  $(".pnav-list").css("top","0");
+});
+
+$(".close-menu").click(function(){
+  $(".pnav-list").css("top","-22rem");
+});
